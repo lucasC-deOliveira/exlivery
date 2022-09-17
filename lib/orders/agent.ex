3b7e606d20af7b0730f0367b337129d65bc4f48d
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-09-17 19:13:59 -0300

    Salvando a Order no Agent

---
+++ lib/orders/agent.ex
@@ -0,0 +1,29 @@
+defmodule Exlivery.Orders.Agent do
+  alias Exlivery.Orders.Order
+
+  use Agent
+
+  # Agent é usado para manter um estado
+  def start_link(_init) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%Order{} = order) do
+    uuid = UUID.uuid4()
+
+    Agent.update(__MODULE__, &update_state(&1, order, uuid))
+
+    {:ok, uuid}
+  end
+
+  defp update_state(state, %Order{} = order, uuid), do: Map.put(state, uuid, order)
+
+  def get(uuid), do: Agent.get(__MODULE__, &get_order(&1, uuid))
+
+  defp get_order(state, uuid) do
+    case Map.get(state, uuid) do
+      nil -> {:error, "User not found"}
+      order -> {:ok, order}
+    end
+  end
+end
--- lib/users/agent.ex
+++ lib/users/agent.ex
@@ -8,7 +8,7 @@ defmodule Exlivery.Users.Agent do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
-  def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, &2))
+  def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, user))
 
   defp update_state(state, %User{cpf: cpf} = user), do: Map.put(state, cpf, user)
 
