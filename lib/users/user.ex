8e77a0c55876856841201eef466f05baeff22c5c
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-09-17 10:38:12 -0300

    Finalizando a struct de Order

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,18 +1,19 @@
 defmodule Exlivery.Users.User do
   # structs é um mapa com nome
-  @keys [:name, :email, :cpf, :age]
+  @keys [:address, :name, :email, :cpf, :age]
   @enforce_keys @keys
   defstruct @keys
 
-  def build(name, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
+  def build(address, name, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
     {:ok,
      %__MODULE__{
        name: name,
        email: email,
+       address: address,
        cpf: cpf,
        age: age
      }}
   end
 
-  def build(_name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
+  def build(_address, _name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
 end
